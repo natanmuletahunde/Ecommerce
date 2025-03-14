a61b2b7f970c0d0b342df46f8f905a25267ff306
pivnati <natanmuleta911@gmail.com>
2025-03-14 16:33:03 -0700

    create the Title.jsx file in the components fodler

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
 
 const LatestCollection = () => {
+    const {products} = useContext(ShopContext)
+     
   return (
-    <div>
-      
+    <div className='my-10'>
+       <div className='text-center py-8 text-3xl'>
+<Title text1={'LATEST'}  text2={'COLLECTIONS'}/>
+       </div>
     </div>
   )
 }
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -4,11 +4,10 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
     const currency = '$'
-
     const delivery_fee= 10;
     const value = {
             products,currency,delivery_fee
-    }; // Define your context value here
+    }; 
 
     return (
         <ShopContext.Provider value={value}>
