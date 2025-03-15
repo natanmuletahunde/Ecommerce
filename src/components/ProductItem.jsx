bec01173d28bcb12391f3fd837ccb7b8c5299143
pivnati <natanmuleta911@gmail.com>
2025-03-14 23:18:50 -0700

    create the Bestseller.jsx files  in the components folder

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
-const ProductItem = ({id,image,name,price}) => {
+import { Link } from "react-router-dom";const ProductItem = ({id,image,name,price}) => {
     const {currency} = useContext(ShopContext)
   return (    
     <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
