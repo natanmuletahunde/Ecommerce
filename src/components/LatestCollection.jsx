7d628fb24a4c8fb7bde16efe8320f740b7df08c1
pivnati <natanmuleta911@gmail.com>
2025-03-14 19:56:09 -0700

    create the productitmes in the components

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,17 +1,21 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title'
 
 const LatestCollection = () => {
-    const {products} = useContext(ShopContext)
-     
+    const {products} = useContext(ShopContext);
+    const {latestProducts,setLatestProducts} = useState([]);
+    useEffect(()=>{
+      setLatestProducts(products.slice(0,10));
+    },[])
   return (
     <div className='my-10'>
        <div className='text-center py-8 text-3xl'>
-<Title text1={'LATEST'}  text2={'COLLECTIONS'}/>
+<          Title text1={'LATEST'}  text2={'COLLECTIONS'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint itaque minima repellendus aliquam architecto pariatur ipsa labore maxime illum laborum exercitationem at, consequatur maiores rerum blanditiis placeat ducimus, assumenda error.</p>
        </div>
     </div>
   )
 }
-
 export default LatestCollection
