4f5f571d5af7a26bd6302d39f372419dff1327a1
pivnati <natanmuleta911@gmail.com>
2025-03-16 08:36:02 -0700

    modify the collection files

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import PlaceOrder from './pages/PlaceOrder';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
         <Route path='/product/:id' element={<Product />} />
         <Route path='/cart' element={<Cart />} />
       </Routes>
+  <Footer/>
 
     </div>
   );
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Collection = () => {
+  const { products } = useContext(ShopContext);
+  const [ShowFilter, setShowFilter] = useState(false);
+
   return (
-    <div>
-      
+    <div className='flex flex-col sm:flex-row items-center gap-1 sm:gap-10 pt-10 border-t'>
+      <div className='min-w-60'></div>
+      <p 
+        className='my-2 text-xl flex items-center cursor-pointer gap-2' 
+        onClick={() => setShowFilter(!ShowFilter)}
+      >
+        FILTERS
+      </p>
+      <div className={`border border-gray-300 pl-5 py-3 mt-6 w-full sm:w-1/3 ${ShowFilter ? 'block' : 'hidden'} sm:block`}>
+        <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+        <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+          <label className='flex gap-2'>
+            <input className='w-3' type="checkbox" value="MEN" /> Men
+          </label>
+          <label className='flex gap-2'>
+            <input className='w-3' type="checkbox" value="WOMEN" /> Women
+          </label>
+          <label className='flex gap-2'>
+            <input className='w-3' type="checkbox" value="KIDS" /> Kids
+          </label>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Collection
+export default Collection;
