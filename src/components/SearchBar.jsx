807698c8cfe6afea48cda9f4ae8167ad78d4d688
pivnati <natanmuleta911@gmail.com>
2025-03-18 16:52:35 -0700

    modfiy the searchbar in the file

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,9 +2,12 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 
 const SearchBar = () => {
-    const {search,setSearch,showSearch,serShowSearch} = useContext(ShopContext)
-  return (
+    const {search,setSearch,showSearch,serShowSearch} = useContext(ShopContext);
+  return  showSearch ? (
     <div>
+      <div>
+        
+      </div>
 
     </div>
   )
