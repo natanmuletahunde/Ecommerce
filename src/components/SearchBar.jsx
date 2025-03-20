f8dfb9732e1b163f6b541aa5a42c7663758a8ed5
pivnati <natanmuleta911@gmail.com>
2025-03-19 17:56:29 -0700

    modfiy the searchbar in the component folder

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,53 +1,18 @@
-import React, { useContext, useEffect } from 'react';
-import { ShopContext } from '../context/ShopContext';
-import { assets } from '../assets/assets';
-import { useLocation } from 'react-router-dom';
-
+import React, {useContext} from "react";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
 const SearchBar = () => {
-  const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
-  const location = useLocation();
-
-  useEffect(()=>{
-    console.log(location.pathname)
-  },[location])
-  // Function to handle search input change
-  const handleSearchChange = (e) => {
-    setSearch(e.target.value);
-  };
-
-  // Function to close the search bar
-  const handleCloseSearch = () => {
-    setShowSearch(false);
-  };
-
-  return showSearch ? (
-    <div className="border-t border-b bg-gray-50 text-center p-4 relative">
-      <div className="inline-flex items-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 relative">
-        {/* Search Input */}
-        <input
-          value={search}
-          onChange={handleSearchChange}
-          type="text"
-          className="flex-1 outline-none bg-inherit text-sm pl-2 pr-10 focus:ring-2 focus:ring-blue-400"
-          placeholder="Search"
-        />
-        {/* Search Icon */}
-        <img
-          src={assets.search_icon}
-          className="w-5 h-5 absolute right-4"
-          alt="Search Icon"
-        />
-      </div>
-
-      {/* Close Icon */}
-      <img
-        onClick={handleCloseSearch}
-        className="absolute top-4 right-4 w-5 h-5 cursor-pointer hover:scale-105 transform transition duration-200"
-        src={assets.cross_icon}
-        alt="Close Search"
-      />
+  const {search, setSearch, showSearch , setShowSearch}= useContext(ShopContext);
+  
+  return showSearch ?  (
+    <div  className="border-t border-b bg-gray-50 text-center">
+         <div className="inline-flex items-center justify-center border border-gray-400  px-2 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
+              <input value={search} onChange={(e)=>setSearch(e.target.value)} type="text" className="flex-1 outline-none bg-inherit text-sm" placeholder="Search" />
+              <img src={assets.search_icon} className="w-4" alt="" />
+          </div>  
+          <img onClick={()=>setShowSearch(false)} src={assets.cross_icon} className="inline w-3 cursor-pointer  " alt="" />    
     </div>
-  ) : null;
-};
+  ):null
+}
 
-export default SearchBar;
+export default SearchBar
