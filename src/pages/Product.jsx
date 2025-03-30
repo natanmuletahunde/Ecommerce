428308655ebe801e61c96615c23d35741ac8d1ae
pivnati <natanmuleta911@gmail.com>
2025-03-30 15:51:34 -0700

    create the product.jsx and modfiy it

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-
+import React, { useContext, useEffect, useState } from 'react'; // Added useState
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/shopContext';
 const Product = () => {
- 
-  return (
-    <div>
-      
-    </div>
-  )
-}
- 
-export default Product
+  const { ProductId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false); // useState is now defined
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === ProductId) {
+        setProductData(item);
+        console.log(item);
+        return null;
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  },[ProductId]);
+};
+
+export default Product;
